d1b899eba40e92f7c575f0806f987d3ddf81b1a1
lechat <ctpeko3a-github@gmail.com>
2012-04-27 14:40:57 +0100

    Working example of the custom resource, provider and cookbook to stop servers in order

--- stop_server/metadata.py
+++ stop_server/metadata.py
@@ -1,7 +1,18 @@
 
 __config__ = {
-    "port": dict(
+    "managed_server_1_port": dict(
         description = "Checks if server is already running on this port",
-        default = [],
+        default = None,
+        required = True
+    ),
+    "managed_server_2_port": dict(
+        description = "Checks if server is already running on this port",
+        default = None,
+        required = True
+    ),
+    "admin_server_port": dict(
+        description = "Checks if server is already running on this port",
+        default = None,
+        required = True
     ),
 }
--- stop_server/recipes/default.py
+++ stop_server/recipes/default.py
@@ -1,17 +1,23 @@
-from kokki import Execute, File, Template
+from kokki import Execute, File, Template, Service
 
-if not env.config.wl_jms:
-    raise AttributeException('wl_jms is not set')
+env.cookbooks.stop_server.SimpleHTTPServer("managed_server_1",
+    service_name = "managed_server_1",
+    action = ['stop'],
+    port = env.config.managed_server_1_port
+    )
 
-File(
-    env.config.file_path,
-    content = Template("simple_file/simple_file.j2", env=env),
-)
+env.cookbooks.stop_server.SimpleHTTPServer("managed_server_2",
+    service_name = "managed_server_2",
+#    stop_command = "echo $s|return 2" % env.config.managed_server_2_port,
+    action = ['stop'],
+    port = env.config.managed_server_2_port
+    )
 
-Execute(
-        'Execute uname',
-        command = 'uname %s' % env.config.uname_param,
-        subscribes = [("run", env.resources["File"][env.config.file_path], True)]
-        )
-
+env.cookbooks.stop_server.SimpleHTTPServer("admin_server",
+    service_name = "SimpleHTTPServer",
+    action = ['stop'],
+    port = env.config.admin_server_port,
+    subscribes = [('stop', env.resources['SimpleHTTPServer']['managed_server_1'], False),
+                ('stop', env.resources['SimpleHTTPServer']['managed_server_2'], False)]
+    )
 
