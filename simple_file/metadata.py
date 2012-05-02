06cd53501ddaad2a1a811409836f42fd02b18ba5
Aleksey Maksimov <ama385@maersk.com>
2012-05-02 19:14:40 +0200

    Added tenjin template to show how it works

--- simple_file/metadata.py
+++ simple_file/metadata.py
@@ -1,16 +1,11 @@
 
 __config__ = {
-    "users": dict(
+    "simple_file.users": dict(
         description = "Users which will appear in file",
         default = [],
     ),
-    "file_path" : dict(
+    "simple_file.file_path" : dict(
         description = 'Path and file name',
         default = '/tmp/test_simple_file',
-    ),
-    'uname_param': dict(
-        description = 'Parameter passes to uname',
-        default = '-a',
-        # default = None,
     )
 }
--- simple_file/recipes/default.py
+++ simple_file/recipes/default.py
@@ -1,15 +1,31 @@
-
 from kokki import Execute, File, Template
+import tenjin
+import re
 
-Execute(
-        'Execute uname',
-        command = 'uname %s' % env.config.uname_param,
-        )
+class NoShellVarSubstTemplate(tenjin.Template):
+    """Same as standard replacemt except that we ignore patters like this: ${...}"""
+    s = '(?:\{.*?\}.*?)*'
+    EXPR_PATTERN = re.compile(r'#\{(.*?' + s + r')\}|\{=(?:=(.*?)=|(.*?))=\}', re.S)
+    del s
 
-if not env.config.wl_jms:
-    raise AttributeException('wl_jms is not set')
+    def __init__(self, filename=None, encoding=None, iinput=None, escapefunc=None, tostrfunc=None,
+                    indent=None, preamble=None, postamble=None, smarttrim=None, trace=None):
+        tenjin.Template.__init__(self, filename, encoding, iinput, escapefunc, tostrfunc,
+                                    indent, preamble, postamble, smarttrim, trace)
+
+    def expr_pattern(self):
+        return self.EXPR_PATTERN
+
+    def get_expr_and_flags(self, match):
+        expr1, expr2, expr3 = match.groups()
+        if expr1:
+            return expr1, (False, True)   # not escape,  call to_str
+        if expr2:
+            return expr2, (False, True)   # not escape,  call to_str
+        if expr3:
+            return expr3, (True,  True)   # call escape, call to_str
 
 File(
     env.config.file_path,
-    content = Template("simple_file/simple_file.j2", env=env),
+    content = Template("simple_file/simple_file.tj", env=env, variables=env.config, engine='tenjin', templateclass=NoShellVarSubstTemplate),
 )
